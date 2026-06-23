dcb71e13e5d97182cebddcd1d972c3c73dbf1df3
Matheus Zuim Campagnol <matheus1411071227@gmail.com>
2026-06-23 00:32:36 -0300

    feat(combat): implementa sistema inicial de batalha com defesa e IA simples
    Implementado sistema de combate inicial do RPG:
    
    - Jogador pode atacar, defender ou fugir
    - Troll com ações aleatórias
    - Sistema básico de dano e vida
    - Flag de defesa para reduzir dano
    - Loop principal de batalha funcionando

--- combate/combate_v1.2.py
+++ combate/combate_v1.2.py
@@ -8,6 +8,8 @@ jogador_vida = 100
 jogador_atk = 25
 jogador_def = 20
 
+jogador_defTF = False
+
 troll_vida = 100
 troll_atk = 20
 troll_def = 15
@@ -16,8 +18,8 @@ troll_def = 15
 while True:
 
     print('════════ COMBATE ════════')
-    print(f'❤️ Jogador: {jogador_vida}')
-    print(f'👹 Troll: {troll_vida}')
+    print(f'Jogador: {jogador_vida}')
+    print(f'Troll: {troll_vida}')
     print('════════════════════════')
 
     jogador = int(input('[ 1 ] Atacar \n[ 2 ] Defender \n[ 3 ] Fugir \n>>>> '))
@@ -25,34 +27,50 @@ while True:
 
     if jogador == 1:
         print('⚔ Você avança contra o Troll!')
-        print('💥 Dano causado: {}'.format(jogador_atk))
+        print('Dano causado: {}'.format(jogador_atk))
         troll_vida -= jogador_atk
-        print('👹 Vida do Troll: {}'.format(troll_vida))
-    if jogador == 2:
-        troll_atk - 10
-    if jogador == 3:
-        print('🏃 Você corre para longe do Troll!')
-        print('🌲 Você escapou para a floresta.')
+        print('Vida do Troll: {}'.format(troll_vida))
+
+    elif jogador == 2:
+        print('Você ergue sua guarda!')
+        jogador_defTF = True
+
+    elif jogador == 3:
+        print('Você corre para longe do Troll!')
+        print('Você escapou para a floresta.')
         print('Combate encerrado.')
         break
 
     if maquina == 1:
-        print('⚔ Troll da Floresta ataca!')
-        print('💥 Dano recebido: {}'.format(troll_atk))
-        jogador_vida -= troll_atk
-        print('👹 Sua vida: {}'.format(jogador_vida))
-    if maquina == 2:
-        jogador_atk - 10
-    if maquina == 3:
-        print('👹 O Troll recua assustado!')
-        print('🌲 Ele desaparece entre as árvores da floresta.')
+        if jogador_defTF == False:
+            print('⚔ Troll da Floresta ataca!')
+            print('Dano recebido: {}'.format(troll_atk))
+            jogador_vida -= troll_atk
+            print('Sua vida: {}'.format(jogador_vida))
+        if jogador_defTF == True:
+            print('⚔ Troll da Floresta ataca!')
+            print('Dano recebido reduzido: {}'.format(troll_atk - 10))
+            jogador_vida -= (troll_atk - 10)
+            print('Sua vida: {}'.format(jogador_vida))
+            jogador_defTF = False
+
+    elif maquina == 2:
+        if jogador == 1:
+            print('O troll bloqueou seu ataque.')
+            print('Dano recebido reduzido: {}'.format(jogador_atk - 10))
+            troll_vida -= (jogador_atk - 10)
+            print('Vida do troll: {}'.format(troll_vida))
+
+    elif maquina == 3:
+        print('O Troll recua assustado!')
+        print('Ele desaparece entre as árvores da floresta.')
         print('Combate encerrado.')
         break
 
     if troll_vida <= 0:
         print('⚔ Você desfere um golpe poderoso!')
-        print('💥 Dano causado: {}'.format(jogador_atk))
-        print('👹 Vida do Troll: {}'.format(troll_vida))
-        print('☠ O Troll da Floresta foi derrotado!')
-        print('🏆 Vitória!')
+        print('Dano causado: {}'.format(jogador_atk))
+        print('Vida do Troll: {}'.format(troll_vida))
+        print('O Troll da Floresta foi derrotado!')
+        print('Vitória!')
         break
